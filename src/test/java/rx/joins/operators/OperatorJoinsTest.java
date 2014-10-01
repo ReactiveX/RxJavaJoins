cba5663c731c105eda7886924140af38015fdbd5
Ben Christensen <benjchristensen@netflix.com>
2014-09-30 22:02:16 -0700

    Upgrade to RxJava 1.0

--- src/test/java/rx/joins/operators/OperatorJoinsTest.java
+++ src/test/java/rx/joins/operators/OperatorJoinsTest.java
@@ -33,7 +33,6 @@ import org.mockito.MockitoAnnotations;
 
 import rx.Observable;
 import rx.Observer;
-import rx.exceptions.TestException;
 import rx.functions.Func0;
 import rx.functions.Func1;
 import rx.functions.Func2;
